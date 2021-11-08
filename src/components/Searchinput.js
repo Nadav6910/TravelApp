481a9bc3f06428ebcca300a2aee31a7875d2f067
Nadav Shor <nadavs@nadavs-air.home>
2021-11-08 03:40:20 +0200

    added search bar

--- src/components/Searchinput.js
+++ src/components/Searchinput.js
@@ -1,10 +1,18 @@
 import "../styling/searchinput.css"
+import { usePlacesWidget } from "react-google-autocomplete";
+
+
 
 export default function Searchinput() {
 
+    const { ref } = usePlacesWidget({
+        apiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+        onPlaceSelected: (place) => console.log(place)
+      })
+
     return (
         <div className="search-input">
-            
+            <input className="search-input" ref={ref} placeholder="Where To.."></input>
         </div>
     )
 }
