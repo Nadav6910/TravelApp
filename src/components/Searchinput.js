251d4e1b68fa85d7f9d2e3a19e97a52b6d2359dc
Nadav Shor <nadavs@Nadavs-MacBook-Air.local>
2021-11-10 19:26:19 +0200

    added input padding and delete button

--- src/components/Searchinput.js
+++ src/components/Searchinput.js
@@ -1,18 +1,24 @@
 import "../styling/searchinput.css"
 import { usePlacesWidget } from "react-google-autocomplete";
+import { useState } from "react";
 
 export default function Searchinput() {
+
+    const [SearchInputValue, SetSearchInputValue] = useState('')
 
     const { ref } = usePlacesWidget({
         apiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
         onPlaceSelected: (place) => console.log(place)
       })
 
+    function DeleteText(){
+        SetSearchInputValue('')
+    }
+
     return (
         <div className="search-input-container">
-            <input className="search-input" ref={ref} placeholder="Where To.."></input>
-            <i class="fas fa-plane"></i>
-            {/* <FlightIcon/> */}
+            <input onChange={e => SetSearchInputValue(e.target.value)} value={SearchInputValue} className="search-input" ref={ref} placeholder="Where To.."></input>
+            {SearchInputValue.length > 0 ? <i onClick={DeleteText} className="fas fa-times-circle"></i> : <i className="fas fa-plane"></i>}
         </div>
     )
 }
