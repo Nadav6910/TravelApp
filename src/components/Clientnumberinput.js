fcf6053466d7193e7b1967d783422eddea28dcb3
Nadav Shor <nadavs@nadavs-air.home>
2021-11-11 22:36:17 +0200

    almost finished work on the choose number of people commponent

--- src/components/Clientnumberinput.js
+++ src/components/Clientnumberinput.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-useless-concat */
 import "../styling/clientnumberinput.css"
 import { useState } from "react"
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle'
+import AddCircleIcon from '@mui/icons-material/AddCircle'
 
 export default function Clientnumberinput(){
 
@@ -8,17 +10,48 @@ export default function Clientnumberinput(){
     const [NumberOfAdults, SetNumberOfAdults] = useState(2)
     const [NumberOfChildren, SetNumberOfChildren] = useState(0)
 
+    function ChangeWindowState(){
+        SetWindowOpen(!WindowOpen)
+    }
+
+    function DecreseNumberAdults(){
+        if (NumberOfAdults > 1){
+            SetNumberOfAdults(NumberOfAdults - 1)
+        }
+    }
+
+    function DecreseNumberChildren(){
+        if (NumberOfChildren > 0){
+            SetNumberOfChildren(NumberOfChildren - 1)
+        }
+    }
+
+    function IncreseNumberAdults(){
+        SetNumberOfAdults(NumberOfAdults + 1)
+}
+
+    function IncreseNumberChildren(){
+            SetNumberOfChildren(NumberOfChildren + 1)
+    }
+
     return (
         <div className="client-number-container">
-            <input onClick={SetWindowOpen} className="number-input" value={NumberOfAdults + ' Adult' + '  -  ' + NumberOfChildren + ' Children' }></input>
+            <input onClick={ChangeWindowState} className="number-input" value={NumberOfAdults + ' Adult' + '  -  ' + NumberOfChildren + ' Children' }></input>
             <i class="fas fa-user"></i>
             {WindowOpen ? <div className="select-number-container">
                 <div className="adults-span-container">
                     <p className="adults-span">Adults</p>
+                    <RemoveCircleIcon onClick={DecreseNumberAdults} className="remove-icon" fontSize="small"/>
+                    <span className="adults-selected">{NumberOfAdults}</span>
+                    <AddCircleIcon onClick={IncreseNumberAdults} className="add-icon" fontSize="small"/>
                 </div>
+                <hr className="line-break"/>
                 <br/>
                 <div className="children-span-container">
                     <p className="children-span">Children</p>
+                    <RemoveCircleIcon onClick={DecreseNumberChildren} className="remove-icon" fontSize="small"/>
+                    <span className="adults-selected">{NumberOfChildren}</span>
+                    <AddCircleIcon onClick={IncreseNumberChildren} className="add-icon" fontSize="small"/>
                 </div>
             </div> : null}
         </div>
