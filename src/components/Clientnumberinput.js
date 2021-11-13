22a1e00e75b1b334c1688cf69d26fba0c1b138ad
Nadav Shor <nadavs@nadavs-air.home>
2021-11-13 20:52:32 +0200

    some minor css fixes

--- src/components/Clientnumberinput.js
+++ src/components/Clientnumberinput.js
@@ -48,6 +48,7 @@ export default function Clientnumberinput(){
 
     return (
         <div ref={ref} className="client-number-container">
+
             <input 
                 onBlur={CloseWindowWhenNotFocused}
                 onClick={ChangeWindowState} 
@@ -55,22 +56,26 @@ export default function Clientnumberinput(){
                 value={NumberOfAdults + ' Adult' + '  -  ' + NumberOfChildren + ' Children' }>
             </input>
             <i class="fas fa-user"></i>
-            {WindowOpen ? <div className="select-number-container">
+
+            {WindowOpen && <div className="select-number-container">
                 <div className="adults-span-container">
                     <p className="adults-span">Adults</p>
                     <RemoveCircleIcon onClick={DecreseNumberAdults} className="remove-icon" fontSize="small"/>
                     <span className="adults-selected">{NumberOfAdults}</span>
                     <AddCircleIcon onClick={IncreseNumberAdults} className="add-icon" fontSize="small"/>
                 </div>
+
                 <hr className="line-break"/>
                 <br/>
+
                 <div className="children-span-container">
                     <p className="children-span">Children</p>
                     <RemoveCircleIcon onClick={DecreseNumberChildren} className="remove-icon" fontSize="small"/>
                     <span className="adults-selected">{NumberOfChildren}</span>
                     <AddCircleIcon onClick={IncreseNumberChildren} className="add-icon" fontSize="small"/>
                 </div>
-            </div> : null}
+
+            </div>}
         </div>
     )
 }
